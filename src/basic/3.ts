ee44a86d235e73e7d1e256b996af3cdd525a5617
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-09-19 22:49:43 +0300

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+// Завдання 3
+
+// Створіть змінну, яка може містити або рядок, або число 
+// (union type)? Також, оголосіть змінну, яка може містити 
+// лише одне з двох можливих рядкових значень: 
+// 'enable' або 'disable' (literal type).
+
+let unionType: string|number;
+let literalType: 'enable' | 'disable';
