ad8488faabb529b2f3023a2994ca671f69d7f7c8
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-09-20 15:06:10 +0300

    generics

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -1,21 +1,42 @@
-Завдання 2
-Виконуйте це завдання у файлі src/generics/2.ts.
+// Завдання 2
 
-У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. Ваше завдання – використовувати Pick та generics для вказівки, що поля цих параметрів належать AllType. Функція compare повинна повертати AllType.
+// У вас є тип AllType. Існує функція compare, яка приймає два 
+// об'єкти. Ці об'єкти містять поля AllType. 
+// Ваше завдання – використовувати Pick та generics для вказівки, 
+// що поля цих параметрів належать AllType. 
+// Функція compare повинна повертати AllType.
+
+// type AllType = {
+//   name: string;
+//   position: number;
+//   color: string;
+//   weight: number
+// }
+
+// function compare (top, bottom): AllType {
+//   return {
+//     name: top.name,
+//     color: top.color,
+//     position: bottom.position,
+//     weight: bottom.weight,
+//   }
+// }
 
 type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
-}
-
-function compare (top, bottom): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
-}
-
+      name: string;
+      position: number;
+      color: string;
+      weight: number
+    }
+    
+    function compare<
+    T extends Pick<AllType, 'name'| 'color'>,
+    K extends Pick<AllType, 'position'| 'weight'> 
+    > (top: T, bottom: K): AllType {
+      return {
+        name: top.name,
+        color: top.color,
+        position: bottom.position,
+        weight: bottom.weight,
+      }
+    }
--- src/generics/6.ts
+++ src/generics/6.ts
@@ -1,23 +1,43 @@
-Завдання 6
-Виконуйте це завдання у файлі src/generics/6.ts.
+// Завдання 6
 
-У вас є тип Form, який містить інформацію про форму, включаючи поле errors. Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
+// У вас є тип Form, який містить інформацію про форму, 
+// включаючи поле errors. Ви хочете створити новий тип Params, 
+// який включає всі поля з Form, крім errors.
 
-type Errors = {
-  email?: string[];
-  firstName?: string[];
-  lastName?: string[];
-  phone?: string[];
-};
+// type Errors = {
+//   email?: string[];
+//   firstName?: string[];
+//   lastName?: string[];
+//   phone?: string[];
+// };
 
-type Form = {
-  email: string | null;
-  firstName: string | null;
-  lastName: string | null;
-  phone: string | null;
-  errors: Errors;
-};
+// type Form = {
+//   email: string | null;
+//   firstName: string | null;
+//   lastName: string | null;
+//   phone: string | null;
+//   errors: Errors;
+// };
 
 // Реалізуйте Params так, 
 // щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+// type Params = Form;
+
+type Errors = {
+    email?: string[];
+    firstName?: string[];
+    lastName?: string[];
+    phone?: string[];
+  };
+  
+  type Form = {
+    email: string | null;
+    firstName: string | null;
+    lastName: string | null;
+    phone: string | null;
+    errors: Errors;
+  };
+  
+  type Params = Omit<Form, "errors">;
+
+  
